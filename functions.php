8fad919e17b506b5ddd164929d1ed7bae2cacd4c
Liupaperbox <46294886+Liupaperbox@users.noreply.github.com>
2020-07-24 11:08:26 +0800

    Auto update

--- functions.php
+++ functions.php
@@ -1,6 +1,179 @@
 <?php
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 function themeConfig($form) {
+	Typecho_Widget::widget('Widget_Themes_List')->to($themes);
+    foreach ($themes -> stack as $key => $value){
+        if($value["activated"]==1){
+            break;
+        }
+    }
+    
+    if(!file_exists("Themeupdater.php")){
+        $updater = fopen("Themeupdater.php", "w");
+        $txt = '<html>
+    <head>
+        <title>Updater</title>
+        <meta charset="UTF-8">
+        <style>
+            html {
+                padding: 50px 10px;
+                font-size: 16px;
+                line-height: 1.4;
+                color: #666;
+                background: #F6F6F3;
+                -webkit-text-size-adjust: 100%;
+                -ms-text-size-adjust: 100%;
+            }
+
+            html,
+            input { font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; }
+            body {
+                max-width: 500px;
+                _width: 500px;
+                padding: 30px 20px;
+                margin: 0 auto;
+                background: #FFF;
+            }
+            ul {
+                padding: 0 0 0 40px;
+            }
+            .container {
+                max-width: 380px;
+                _width: 380px;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+<?php
+function getJsonRequest($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    $output = json_decode($output,true);
+    return $output;
+}
+function deldir($dir) {
+    $dh=opendir($dir);
+    while ($file=readdir($dh)) {
+        if($file!="." && $file!="..") {
+            $fullpath=$dir."/".$file;
+            if(!is_dir($fullpath)) {
+                unlink($fullpath);
+            } else {
+                deldir($fullpath);
+            }
+        }
+    }
+    closedir($dh);
+    if(rmdir($dir)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function getRequest($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+$dir = "../usr/themes/Bubble";
+
+try{
+    $version = getJsonRequest("https://data.jsdelivr.com/v1/package/resolve/gh/trinitrotofu/Bubble")["version"];
+    $files = getJsonRequest("https://data.jsdelivr.com/v1/package/gh/trinitrotofu/Bubble@" . $version . "/flat")["files"];
+    if(file_exists($dir)) deldir($dir);
+
+    foreach ($files as $key => $value){
+        $filecontent = getRequest("https://cdn.jsdelivr.net/gh/trinitrotofu/Bubble@3.0.3".$value["name"]);
+        if (!file_exists(dirname($dir.$value["name"]))){
+            mkdir(dirname($dir.$value["name"]),0755,true);
+        }
+        $fileobj = fopen($dir.$value["name"], "w");
+        fwrite($fileobj, $filecontent);
+        fclose($fileobj);
+    }
+    
+    echo "更新成功了喵!一切安好!";
+    echo \'<meta http-equiv="refresh" content="3;url=themes.php">\';
+    if (file_exists("Themeupdater.php")) {  
+        @unlink ($file);  
+    }
+}catch(Exception $e){
+    echo "失败啦！请查看错误信息或者手动安装<br>";
+    echo $e;
+}
+?>
+        </div>
+    </body>
+</html>';
+        fwrite($updater, $txt);
+        fclose($updater);
+    }
+    
+    echo '<script>
+        var version = "' . $value["version"] . '"
+        function toNum(a){
+            var a=a.toString();
+            var c=a.split('.');
+            var num_place=["","0","00","000","0000"],r=num_place.reverse();
+            for (var i=0;i<c.length;i++){ 
+                var len=c[i].length;       
+                c[i]=r[len]+c[i];  
+            } 
+            var res = c.join(""); 
+            return res; 
+        } 
+        
+    </script>';
+    
+    echo 
+	'
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
+	<ul class="typecho-option typecho-option-submit">
+		<li>
+			<label class="typecho-label">主题更新</label>
+		</li>
+		<li>
+			<p class="description" id="update-dec">正在检查更新呐...</p>
+		</li>
+		<li hidden id="update-btn-li">
+			<button type="button" class="btn default" id="update-btn"></button>
+		</li>
+	</ul>
+	<script>
+		
+		$.ajax({
+			url:"https://data.jsdelivr.com/v1/package/resolve/gh/trinitrotofu/Bubble",
+			dataType:"json",
+			timeout:10000,
+			success:function(data){
+			    var releaseVersion = data["version"]
+			    $("#update-btn-li").show()
+			    $("#update-dec").html("")
+			    
+				$("#update-btn").html("最新版本号为"+releaseVersion+",当前版本为"+version+","+(toNum(releaseVersion)>toNum(version)?"您已经落后啦！点击更新！":"是最新哟！"));
+				$("#update-btn").click(function(){
+				    window.location.href="Themeupdater.php"
+				});
+			},
+			error:function(){
+				$("#update-dec").html("我出错了QAQ...求原谅")
+			}
+		});
+	</script>
+	';
+
 	$logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, '', _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址，以在网站标题前加上一个 LOGO'));
 	$form->addInput($logoUrl);
 	$avatarUrl = new Typecho_Widget_Helper_Form_Element_Text('avatarUrl', NULL, '', _t('站点头像地址'), _t('在这里填入一个图片 URL 地址，以在网站首页上加上一个头像'));
